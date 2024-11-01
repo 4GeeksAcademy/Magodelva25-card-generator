20adbc70ccc63b29b81301fe4ce8a8b1474c014b
Mangodev <migudz@gmail.com>
2024-11-01 13:41:31 +0000

    Actualizadas funcionalidades

--- src/app.js
+++ src/app.js
@@ -8,43 +8,72 @@ import "./assets/img/4geeks.ico";
 import { read } from "@popperjs/core";
 
 const button = document.getElementById("buttonToGenerate");
+const resetButton = document.getElementById("restart");
+
+const figures = ["♠", "♣", "♦", "♥"];
+const values = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2];
+
+//Creamos la baraja, base de 52 cartas para el póker
+const deck = [];
+for (let figure of figures) {
+  for (let value of values) {
+    deck.push({ figure, value });
+  }
+}
+
+// Barajar
+let shuffledDeck = deck.sort(() => 0.5 - Math.random());
+
+let cardCounter = 0;
 
 button.onclick = function() {
-  //Asiganmos una variable a cada glifo
-  const spade = "♠";
-  const trebol = "♣";
-  const diamond = "♦";
-  const hearts = "♥";
+  // Desactivar el generador de cartas al llegar al máximo
+  if (cardCounter >= 52) {
+    alert("No quedan cartas en el mazo");
+    button.disabled = true;
+    return;
+  }
+  // Genramos cartas
+  const currentCard = shuffledDeck[cardCounter];
+  const { figure, value } = currentCard;
 
-  //Randomizar los elementos
-  const cardNumbers = ["A", "K", "Q", "J", 9, 8, 7, 6, 5, 4, 3, 2, 1];
-  const cardFigures = [hearts, spade, diamond, trebol];
-  const randomizedNumber = Math.floor(Math.random() * cardNumbers.length);
-  const randomizedFigures = Math.floor(Math.random() * cardFigures.length);
+  // Establecer estilo rojo para corazones y diamantes
 
-  const randomizedNumberValue = cardNumbers[randomizedNumber];
-  const randomizedFiguresValue = cardFigures[randomizedFigures];
+  const iconStyle = figure === "♥" || figure === "♦" ? "red" : "figuras";
+  const cardValueStyle =
+    figure === "♥" || figure === "♦" ? "values-red" : "values";
 
-  //Añadir el rojo
-  let iconStyle = "icons";
-  if (randomizedFiguresValue === hearts) iconStyle = "red";
-  if (randomizedFiguresValue === diamond) iconStyle = "red";
-  let cardValue = "values";
-  if (randomizedFiguresValue === hearts) cardValue = "values-red";
-  if (randomizedFiguresValue === diamond) cardValue = "values-red";
-
-  // Guardar el estado anterior
-
+  // Guardar el contenido actual de `main-card` en `previous-card`
   const mainCardContent = document.querySelector("#mainCard").innerHTML;
   document.querySelector("#previous-card").innerHTML = mainCardContent;
 
-  //Renderizamos el contenido en el html
+  // Renderizar la carta en `main-card`
+  document.querySelector("#mainCard").innerHTML = `
+    <div class="${iconStyle}">${figure}</div>
+    <div class="${cardValueStyle}">${value}</div>
+    <div class="${iconStyle}">${figure}</div>
+  `;
 
-  const renderElements = (document.querySelector(
-    "#mainCard"
-  ).innerHTML = `<div class=${iconStyle}>${randomizedFiguresValue}</div>
-         <div class=${cardValue}>${randomizedNumberValue}</div>
-         <div class=${iconStyle}>${[randomizedFiguresValue]}</div>`);
+  // Incrementar el contador de cartas
+  cardCounter++;
+  console.log(document.querySelector("#mainCard").innerHTML);
+};
 
-  return renderElements;
+// Aquí la magia del reinicio
+
+resetButton.onclick = function() {
+  alert("Se han reiniciado las cartas");
+
+  cardCounter = 0;
+  shuffledDeck = deck.sort(() => 0.5 - Math.random());
+
+  document.querySelector("#mainCard").innerHTML = `
+    <div class="joker">
+    </div>
+  `;
+  document.querySelector("#previous-card").innerHTML = "";
+  // Reactivar el botón
+  button.disabled = false;
+
+  console.log(document.querySelector("#mainCard").innerHTML);
 };
